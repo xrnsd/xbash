6a0d8df3f2a67a543b93f3118cc943e7ec25fc76
wgx <lz_wuguoxian@163.com>
2016-12-12 18:20:17 +0800

    补充 xbash初始化demo实现

--- module/init_xbah.sh
+++ module/init_xbah.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 ftXrnsdExtensionsToBashInit()
 {
+# ===========             预定流程                =======================
+#
+#
+# 	1 复制工程文件到新目录cmds
+#	2 读取用户密码，更新cmds的value配置
+#	3 备份原始的普通用户的bash配置到cmds/dtata/bash_base_backup
+#	    备份root用户的bash配置到cmds/dtata/bash_root_backup
+#	4 确认3操作成功，更新对应用户的bash配置
+# 	5 初始化完成
+#
+#
+
 	local ftName=xbash配置初始化
 	local userName=`who | awk '{print $1}'|sort -u`
-	local dirPathHome=/home/${userName}
 
 	# 特使实现，对外没有特殊要求
 
@@ -24,6 +35,7 @@ ftXrnsdExtensionsToBashInit()
 	\033[0m"
 		exit
 	fi
+	local dirPathHome=/home/${userName}
 	local dirPathPwd=$(cd `dirname $0`; pwd)/
 	local dirPathXbashBase=$(dirname $dirPathPwd)
 	local dirNameXbashTraget=cmds
@@ -51,7 +63,7 @@ ftXrnsdExtensionsToBashInit()
 	fi
 	# sed -i 's/被替换的内容/要替换成的内容/' file
 	local fileNameXbashTragetDataValue=vlaue
-	local filePathXbashTragetDataValue=${dirPathXbashTragetData}/$fileNameXbashTragetDataValue
+	local filePathXbashTragetDataValue=${dirPathXbashTragetData}/${fileNameXbashTragetDataValue}
 	if [ ! -f $filePathXbashTragetDataValue ];then
 		echo -e "\033[1;31m[${ftName}]错误[filePathXbashTragetDataValue=$filePathXbashTragetDataValue]\033[0m"
 		exit
@@ -66,10 +78,40 @@ ftXrnsdExtensionsToBashInit()
 	done
 	sed -i "s/mUserPwdBase=/mUserPwdBase=$passwd/" $filePathXbashTragetDataValue
 
-	#更新bash配置文件
+	# 备份bash配置文件
+	local fileNameXbashTragetDataBashBaseBackup=bashrc_base_backup
+	local fileNameXbashTragetDataBashRootBackup=bashrc_root_backup
+	local filePathXbashTragetDataBashBaseBackup=${dirPathXbashTragetData}/${fileNameXbashTragetDataBashBaseBackup}
+	local filePathXbashTragetDataBashRootBackup=${dirPathXbashTragetData}/${fileNameXbashTragetDataBashRootBackup}
+	local filePathBashBase=/home/${userName}/.bashrc
+	local filePathBashRoot=/root/.bashrc
+	mv $filePathBashBase ${filePathBashBase}_base_backup
+	cp -rf -v ${filePathBashBase}_base_backup $filePathXbashTragetDataBashBaseBackup
 
+	echo $passwd | sudo -S mv $filePathBashRoot ${filePathBashRoot}_root_backup
+	echo $passwd | sudo -S cp -rf  -v ${filePathBashRoot}_root_backup $filePathXbashTragetDataBashRootBackup
+
+	if [ ! -f $filePathXbashTragetDataBashRootBackup ];then
+		echo -e "\033[1;31m[${ftName}]错误[filePathXbashTragetDataBashRootBackup=$filePathXbashTragetDataBashRootBackup]\033[0m"
+		exit
+	fi
+	#更新bash配置文件
+	local fileNameXbashTragetConfig=config
+	local fileNameXbashTragetConfigBashBase=bashrc_work_lz
+	local fileNameXbashTragetConfigBashRoot=bashrc_root_work_lz
+	local filePathXbashTragetConfig=${dirPathXbashTraget}/${fileNameXbashTragetConfig}
+	local filePathXbashTragetConfigBashBase=${filePathXbashTragetConfig}/${fileNameXbashTragetConfigBashBase}
+	local filePathXbashTragetConfigBashRoot=${filePathXbashTragetConfig}/${fileNameXbashTragetConfigBashRoot}
+
+	cd /home/${userName}&& ln -sf $filePathXbashTragetConfigBashBase .bashrc
+
+	echo $passwd|sudo -S su
+	echo $passwd|sudo -S su<< EOF
+	chown root;root $filePathXbashTragetConfigBashRoot
+	ln -sf $filePathXbashTragetConfigBashRoot /root/.bashrc
+	EOF
 	# 清除无效的文件
-	rm -rf dirPathXbashBase
+	rm -rf $dirPathXbashBase
 }
 
 ftXrnsdExtensionsToBashInit
