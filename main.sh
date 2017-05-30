c2f7ebd504154990abe941a94bcae03b88493ca5
wgx_home <ziliaokuxian@163.com/15019962261/qq:1718686576>
2017-05-31 00:23:00 +0800

    版本 V0.1.0.0 > V0.1.1.0

--- main.sh
+++ main.sh
@@ -34,7 +34,7 @@ ftWhoAmI()## #命令权限判定
     local ftEffect=命令权限判定
     local cmdName=$1
 
-    #耦合变量校验
+    #耦合校验
 
     if [ -z "$cmdName" ];then
         ftReadMe $XCMD
@@ -107,7 +107,6 @@ ftMain()
                 case $rBaseShellParameter2 in
                     "mtk_flashtool")    ftMtkFlashTool ; break;;
                     "restartadb")        ftRestartAdb; break;;
-                    "pac")            ftCopySprdPacFileList; break;;
                     "monkey")        ftKillPhoneAppByPackageName com.android.commands.monkey; break;;
                     "systemui")        ftKillPhoneAppByPackageName com.android.systemui; break;;
                     "launcher")        ftKillPhoneAppByPackageName com.android.launcher3; break;;
@@ -166,7 +165,6 @@ ftReadMe()
         ft | -ft )
     cat<<EOF
 ftKillPhoneAppByPackageName ---- kill掉包名为packageName的应用
-ftCopySprdPacFileList ---------- 自动复制sprd的pac相关文件
 ftBackupOutsByMove               备份out
 ftCleanDataGarbage ------------- 快速清空回收站
 ftReduceFileList                 精简动画帧文件
@@ -175,7 +173,6 @@ ftBootAnimation                  生成开关机动画
 ftAutoUploadPro ---------------- 上传文件到服务器[低耦合版]
 ftLanguageUtils                  语言缩写转换
 ftMtkFlashTool ----------------- mtk下载工具
-ftRmNormalBin                    清空pac相关资源文件
 ftAutoUpload ------------------- 上传文件到固定服务器
 ftUpdateHosts ------------------ 更新hosts
 ftReNameFile ------------------- 批量重命名
@@ -277,11 +274,6 @@ ftAutoUpload 上传文件到固定服务器
     |
     |//ftAutoUpload 上传源文件路径
     |ftAutoUpload /home/xxx/1.test
-
-
-ftRmNormalBin 清空pac相关资源文件
-    |
-    |ftRmNormalBin [dir_path_pac_res] #生成7731c使用的pac的目录，和生成所需的文件存放的目录
 
 
 ftAutoPacket 生成7731c使用的pac
--- module/test/base.sh
+++ module/test/base.sh
@@ -13,116 +13,20 @@ else
 fi
 
 dirNameDebug=temp
-dirPathHome=/home/${rNameUser}/${dirNameDebug}
-dirPathHomeDebug=/home/${rNameUser}/${dirNameDebug}
+dirPathHome=/home/${rNameUser}
+dirPathHomeDebug=${dirPathHome}/${dirNameDebug}
 if [ -d $dirPathHome ];then
-    if [ ! -d $dirPathDebug ];then
-        mkdir  $dirPathDebug
-        ftEcho -s 测试用目录[$dirPathDebug]不存在，已新建
+    if [ ! -d $dirPathHomeDebug ];then
+        mkdir  $dirPathHomeDebug
+        ftEcho -s 测试用目录[$dirPathHomeDebug]不存在，已新建
     fi
-    cd $dirPathDebug
+    cd $dirPathHomeDebug
 else
     echo -e "\033[1;31m    初始化demo环境失败\n\
     模块=$rModuleName\n\
     dirPathHome=$dirPathHome\n\
     \033[0m"
 fi
-
-ftDebug()
-{
-    local ftEffect=调试用，实时跟踪变量变化
-
-    #使用示例
-    while true; do case "$1" in    h | H |-h | -H) cat<<EOF
-#=================== [ ${ftEffect} ]的使用示例===================
-#
-#     trap 'echo “行:$LINENO, a=$a,b=$b,c=$c”' DEBUG
-#     根据需要修改 a，b，c
-#    rIsDebug设为true
-#     ftDebug [任意命令]
-#     ftDebug echo test
-#=========================================================
-EOF
-    if [ "$XMODULE" = "env" ];then
-        return
-    fi
-    exit;; * ) break;; esac;done
-
-    #耦合变量校验
-    if [ -z "$rIsDebug" ];then
-        ftEcho -eax "函数[${ftEffect}]的参数错误 \
-            [参数数量def=$valCount]valCount=$# \
-            rIsDebug=$rIsDebug"
-    fi
-
-     # if [ "$rIsDebug" = "true" ]; then
-        $@
-        trap 'echo “行:$LINENO, path_avail=$path_avail”' DEBUG
-    #  else
-    #      ftEcho -ex 当前非调试模式
-    # fi
-
-}
-
-
-
-# complete -W "example example" ftExample
-ftExample()
-{
-    local ftEffect=函数模板
-    local isSecondTime=false
-
-    #使用示例
-    while true; do case "$1" in
-    #使用环境说明
-    e | -e |--env) cat<<EOF
-#=================== ${ftEffect}使用环境说明=============
-#
-#    工具依赖包 example
-#=========================================================
-EOF
-      return;;
-    #方法使用说明
-    h | H |-h | -H) cat<<EOF
-#=================== [ ${ftEffect} ]的使用示例=============
-#
-#    ftExample 无参
-#    ftExample [example]
-#=========================================================
-EOF
-    if [ "$XMODULE" = "env" ];then
-        return
-    fi
-    exit;;
-    #出现错误之后的尝试
-    x | X |-x | -X)
-        isSecondTime=true
-        ftEcho -s "尝试重新开始 [ftEffect]"
-    break;;
-    * ) break;;esac;done
-
-    #工具环境校验校验
-    if [ -z `which example` ]||[ -z `which example` ];then
-        ftExample -e
-    fi
-    #耦合变量校验
-    local valCount=1
-    if(( $#!=$valCount ))||[ -z "$example1" ]\
-                ||[ -z "$example2" ];then
-        ftEcho -ea "[${ftEffect}]的参数错误 \
-            [参数数量def=$valCount]valCount=$# \
-            [示例1]example1=$example1 \
-            请查看下面说明:"
-        if [ $isSecondTime = "false" ];then
-            ftExample -x
-        fi
-        ftExample -h
-        return
-    fi
-}
-
-
-
 
 # -eq =     # -ne !=
 # -gt >        # -lt <
@@ -131,17 +35,21 @@ EOF
 # if [ $test1 = "1" -o $test2 = "1" ]&&[ $test3 = "3" -a $test4 = "4" ]
 # -o 或    # -a 与
 
+# echo 文件名: ${file%.*}”
+# echo 后缀名: ${file##*.}”
 # ${dirPathFileList%/*}父目录路径
 # ${dirPathFileList##*/}父目录名
 # `basename /home/wgx` wgx
 # `dirname /home/wgx` /home
-# echo 文件名: ${file%.*}”
-# echo 后缀名: ${file##*.}”
-# sed -i 's/被替换的内容/要替换成的内容/' file #内容包含空格需要转义
+#sed -i 's/被替换的内容/要替换成的内容/' file #内容包含空格需要转义
 #sed -i "s:被替换的内容:要替换成的内容:g" file #被替换的内容为路径，内容包含空格需要转义
 #filenumbers= ls -l /media/data_self/backup/os |grep '.tgz'|grep "^-"|wc -l #文件数量获取
-#b=${a//123/321};将${a}里的所有123替换为321\
+#b=${a//123/321} #将${a}里的所有123替换为321\
+# versionName=$(echo $versionName |sed s/[[:space:]]//g) #删除所有空格
+# LOWERCASE=$(echo $VARIABLE | tr '[A-Z]' '[a-z]') #转小写
 # 分割字符串成数组bnList=$(echo $branchName|tr ")" "\n")
+
+# 字符串包含if [[ helloworld == *low* ]];then
 
 # note=${note:-'常规'}
 
@@ -151,6 +59,16 @@ EOF
 #    echo $line
 #done
 
+# 倒序遍历数组
+# i=${#hashLIst[@]}
+# while [ $i -gt "0" ]
+# do
+#   let i--
+#   #${hashLIst[$i]}
+# done
+
+# 根据包名 过滤log
+# adb logcat -v process | grep $(adb shell ps | grep com.android.systemui | awk '{print $2}')
 mTimingStart=$(date +%s -d $(date +"%H:%M:%S"))
 # ===================================================================================================================================
 #####-------------------------------------------------------------------------#########
