8fe5d03d16c12c8b40adb4a5c5490412c70a4768
wgx <lz_wuguoxian@163.com/15019962261/qq:1718686576>
2017-10-08 15:54:21 +0800

    添加 版本号大小比对demo实现

--- module/tools.sh
+++ module/tools.sh
@@ -1870,7 +1870,7 @@ EOF
 complete -W "-a" ftAutoPacket
 ftAutoPacket()
 {
-    local ftEffect=基于android的out生成版本软件
+    local ftEffect=基于android的out生成版本软件包
     local dirPathCode=$ANDROID_BUILD_TOP
     local dirPathOut=$ANDROID_PRODUCT_OUT
     local buildType=$TARGET_BUILD_VARIANT
@@ -2955,33 +2955,43 @@ ftSetBashPs1ByGitBranch()
 }
 
 #版本号大小对比
+ftVersionComparison()
+{
+    local ftEffect=版本号大小比对beta
 
-# -eq =     -ne !=
-# -gt  >      ge >=
-# -lt   <      le <=
+    while true; do case "$1" in
+    h | H |-h | -H) cat<<EOF
+#===================[   ${ftEffect}   ]的使用示例==============
+#
+#    ftVersionComparison -h    #查看帮助
+#
+#    只能比对xx.xx格式的版本号
+#    echo $(ftVersionComparison 版本1 版本2)   #比对版本1和2大小
+#=========================================================
+EOF
+    if [ "$XMODULE" = "env" ];then    return ; fi; exit;;
+    * ) break;;esac;done
 
-function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
-function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
-function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
-function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+    #耦合校验
+    local valCount=2
+    local errorContent=
+    if (( $#!=$valCount ));then    errorContent="${errorContent}\\n[参数数量def=$valCount]valCount=$#" ; fi
+    if [ ! -z "$errorContent" ];then
+            ftEcho -ea "函数[${ftEffect}]的参数错误${errorContent}\\n请查看下面说明:"
+            ftVersionComparison -h
+            return
+    fi
 
-# VERSION="  1.9.1"
-# VERSION2="   2.2"
-# if version_gt $VERSION $VERSION2; then
-#    echo "$VERSION is greater than $VERSION2"
-# fi
-
-# if version_le $VERSION $VERSION2; then
-#    echo "$VERSION is less than or equal to $VERSION2"
-# fi
-
-# if version_lt $VERSION $VERSION2; then
-#    echo "$VERSION is less than $VERSION2"
-# fi
-
-# if version_ge $VERSION $VERSION2; then
-#    echo "$VERSION is greater than or equal to $VERSION2"
-# fi
+    if [[ "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1" ]]; then
+        echo ">"
+    elif [[ "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1" ]]; then
+        echo "<"
+    elif [[ "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1" ]]; then
+        echo "<="
+    elif [[ "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1" ]]; then
+        echo ">="
+    fi
+}
 
 ftAutoInitEnv()
 {
