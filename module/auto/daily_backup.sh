302ec045448e751c876981490906e9718c6f089a
mynameAir <xiangrennengshandu@gmail.com>
2026-01-11 16:03:38 +0800

    change normal update

--- module/auto/daily_backup.sh
+++ module/auto/daily_backup.sh
@@ -8,7 +8,7 @@ if [ -d "$DESTINATION" ]; then
     # 使用 rsync 进行增量备份
     # -a: 归档模式, -v: 详细, -z: 压缩, --delete: 删除目标中源目录已删除的文件
     rsync -avz --delete "$SOURCE" "$DESTINATION"
-    echo "${SOURCE} 备份于 $(date) 成功完成" >> ~/backup_log.txt
+    echo "${SOURCE} 备份于 $(date) 成功完成" >> ~/.backup_log.txt
 else
-    echo "备份失败：未找到移动硬盘 $(date)" >> ~/backup_log.txt
+    echo "备份失败：未找到移动硬盘 $(date)" >> ~/.backup_log.txt
 fi
